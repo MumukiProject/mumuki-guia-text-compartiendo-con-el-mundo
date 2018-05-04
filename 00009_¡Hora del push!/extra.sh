ba779541d35ab15a497709d826eddcffeaf26a50
agus@mumuki.org <agus@mumuki.org>
2018-05-04 13:56:53 -0400

    Mumuki Export on 2018-05-04 13:56:53 -0400

--- 00009_¡Hora del push!/extra.sh
+++ 00009_¡Hora del push!/extra.sh
@@ -10,6 +10,6 @@ function git {
   REMOTE=$2
   BRANCH=$3
   [[ "$ACTION" == "push" ]] && [[ "$REMOTE" == "origin" ]] && [[ "$BRANCH" == "master" ]] && [[ $STATUS = *"nothing to commit"* ]] &&
-  echo "El repositorio se actualizó correctamente" || /usr/bin/git $@
+  raise error "No tenés permisos para actualizar el repositorio en Github" || /usr/bin/git $@
 }
 export -f git
